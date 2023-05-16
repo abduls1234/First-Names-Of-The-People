360b805ca4fbd04f18be2bd3de1ddf1b464094d3
abduls1234 <abdulsamad3125@gmail.com>
2023-05-16 08:52:13 +0000

    first-names-of-the-people

---
+++ names/index.js
@@ -0,0 +1,9 @@
+const peoplesNames = require("../country/state/city/index");
+
+const getFirstNames = require("../utilities/utils/index");
+
+const getPeopleInCity = (peoplesNames) => {
+  return getFirstNames(peoplesNames);
+};
+
+module.exports = getPeopleInCity;
